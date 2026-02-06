c930d394013c62e931f3e58b9427fd21dd8e22b0
dexterrkk3 <rkeithk@mtaonline.net>
2026-02-05 15:58:33 -0900

    initial

---
+++ Bee Game/Assets/Code/Bee.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Bee
+{
+    private float hp;
+    private float maxHp;
+    private float damage; 
+    private float dodgeChance; 
+    private float moveSpeed;
+    private float flySpeed;
+    private bool alive;
+    public Bee(float hp, float damage, float dodgeChance, float moveSpeed, float flySpeed)
+    {
+        this.hp = hp;
+        maxHp = hp;
+        this.damage = damage;
+        this.dodgeChance = dodgeChance;
+        this.moveSpeed = moveSpeed;
+        this.flySpeed = flySpeed;
+        alive = true;
+    }
+    public void takeDamage(float damage)
+    {
+        float x = Random.Range(0, 100);
+        if (x < dodgeChance)
+        {
+            hp -= damage;
+        }
+        if(hp <= 0)
+        {
+            die();
+        }
+    }
+    private void die()
+    {
+        alive = false;
+    }
+}
---
+++ Bee Game/Assets/Code/Worker Bee.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WorkerBee : Bee
+{
+    bool containsPollen;
+    int carryWeight;
+    int pollenAmount; 
+    public WorkerBee(int maxCarryWeight, float hp, float damage, float dodgeChance, float moveSpeed, float flySpeed) : base(hp, damage, dodgeChance, moveSpeed, flySpeed)
+    {
+        containsPollen = false;
+        carryWeight = maxCarryWeight;
+    }
+    public void harvestFlower(int pollenFlower)
+    {
+        containsPollen = true;
+        if(carryWeight +pollenAmount> pollenFlower)
+        {
+            pollenAmount = carryWeight;
+        }
+        else
+        {
+            pollenAmount = pollenFlower;
+        }
+    }
+}
