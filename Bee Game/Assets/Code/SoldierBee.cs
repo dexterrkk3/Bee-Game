4fc0d32f0d72733166f5c9c30b5de4e17e0c3ec4
dexterrkk3 <rkeithk@mtaonline.net>
2026-02-05 17:34:59 -0900

    bee soldier created

---
+++ Bee Game/Assets/Code/SoldierBee.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SoldierBee : WorkerBee
+{
+    public SoldierBee(int maxCarryWeight, float hp, float damage, float dodgeChance, float moveSpeed, float flySpeed) : base(maxCarryWeight, hp, damage, dodgeChance, moveSpeed, flySpeed)
+    {
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
